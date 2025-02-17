73e1ea38c282d000c68d3aaca53a517750874956
222230 <222230@1220-001-22>
2025-02-17 15:24:25 +0300

    сделана 2 практика

--- kvolikblazor/Program.cs
+++ kvolikblazor/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7294/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7294/") });
 
 var app = builder.Build();
 
